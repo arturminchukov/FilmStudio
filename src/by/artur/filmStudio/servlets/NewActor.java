814d13fd93f8349674479c6538b8d25cab8250de
Artur <artur_minchukov@mail.ru>
2017-10-22 02:27:11 +0300

    Added form of registration new Actor with using AJAX

---
+++ src/by/artur/filmStudio/servlets/NewActor.java
@@ -0,0 +1,37 @@
+package by.artur.filmStudio.servlets;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class NewActor
+ */
+@WebServlet("/NewActor")
+public class NewActor extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	public void init() throws ServletException {
+		// TODO Auto-generated method stub
+		super.init();
+	}
+
+	@Override
+	protected void service(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8");
+		response.setCharacterEncoding("UTF-8");
+
+	}
+
+	@Override
+	public void destroy() {
+		// TODO Auto-generated method stub
+		super.destroy();
+	}
+
+}
